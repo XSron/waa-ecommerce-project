818ddfdd031200c5e2bc232b873e1932f9499405
XSron <houxsron@gmail.com>
2020-05-08 19:46:48 -0500

    Finish Add to Cart

--- src/main/java/ecommerce/controller/BuyerController.java
+++ src/main/java/ecommerce/controller/BuyerController.java
@@ -1,6 +1,8 @@
 package ecommerce.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,18 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import ecommerce.model.Product;
 import ecommerce.model.User;
 import ecommerce.service.OrderService;
+import ecommerce.service.ProductService;
 import ecommerce.service.UserService;
 
 @Controller
 @RequestMapping("/buyer")
+@SessionAttributes({"cart"})
 public class BuyerController {
 	@Autowired
 	private OrderService orderService;
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private ProductService productService;
 	
 	@GetMapping("/history")
 	public String orderHistory(Model model, Principal principal) {
@@ -33,5 +42,17 @@ public class BuyerController {
 		model.addAttribute("orders", orderService.findOrderById(id));
 		model.addAttribute("total", orderService.findTotalOrderAmountByOrderId(id));
 		return "buyer/history-detail";
+	}
+	
+	@SuppressWarnings("unchecked")
+	@GetMapping("/addtocart/{productId}")
+	public String addToCart(@PathVariable("productId") Long id, Model model, RedirectAttributes redirectAttributes) {
+		List<Product> products = new ArrayList<>();
+		if(model.getAttribute("cart") != null) 
+			products = (List<Product>) model.getAttribute("cart");
+		products.add(productService.findProductById(id));
+		model.addAttribute("cart", products);
+		redirectAttributes.addFlashAttribute("totalcart", products.size());
+		return "redirect:/";
 	}
 }
